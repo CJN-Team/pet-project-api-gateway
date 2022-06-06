c538e9b3ae40104df2da4f68d7e2d9bdb36d3dee
James Morales <jamesmoralesmoreno@gmail.com>
2022-06-05 23:11:29 -0500

    feat(auth0): auth0 jwt validation

---
+++ Pet.Project.Api.Gateway.API/Handlers/HasScopeHandler.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Pet.Project.Api.Gateway.API.Dtos;
+
+namespace Pet.Project.Api.Gateway.API.Handlers
+{
+    public class HasScopeHandler : AuthorizationHandler<HasScopeRequirement>
+    {
+        protected override Task HandleRequirementAsync(
+            AuthorizationHandlerContext context,
+            HasScopeRequirement requirement
+        )
+        {
+            if (!context.User.HasClaim(claim => claim.Type == "scope" && claim.Issuer == requirement.Issuer))
+            {
+                return Task.CompletedTask;
+            }
+
+            var scopes = context.User.FindFirst(match: scope => scope.Type == "scope" && scope.Issuer == requirement.Issuer)!.Value
+                .Split(' ');
+
+            if (scopes.Any(scope => scope == requirement.Scope))
+            {
+                context.Succeed(requirement);
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
